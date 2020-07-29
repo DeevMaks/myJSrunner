7199f855af19081a0c927c129aa155d644de1dc5
Victor Kasap <victorkasap@Galactus.local>
2020-07-29 21:08:51 +0300

    webpack and git

---
+++ build/webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const PATHS = {
+	src: path.join(__dirname, "../src"),
+	dist: path.join(__dirname, "../dist"),
+	assets: "assets/"
+}
+
+module.exports = {
+	externals : { paths: PATHS },
+	entry: { app: PATHS.src },
+	output: {
+		filename: '[name].bundle.js',
+		path: PATHS.dist,
+		publicPath : '/'
+	},
+	optimization: {
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					name: 'vendors',
+					test: '/node_modules/',
+					chunks: 'all',
+					enforce: true,
+				}
+			}
+		}
+	},
+	module: {
+		rules : [
+			{
+				test: /\.js$/,
+	      		exclude: [ /(node_modules|dist)/, ],
+	      		loader: 'eslint-loader',
+      		},
+      		{
+		      test: /\.(png|jpe?g|gif|svg|webp)$/i,
+		      loader: 'file-loader',
+		      options: { name: '[name].[ext]' }
+		    },
+      	]
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+	    new CopyPlugin({
+	      patterns: [
+	        { from: `${PATHS.src}/${PATHS.assets}images`, to: `${PATHS.assets}images` },
+	      ],
+	    }),
+	    new HtmlWebpackPlugin()
+	  ],
+};
